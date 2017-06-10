1c3e3c7a9a179da7d85e83ea1977112a54f33d14
Chris <ccooper4@hotmail.co.uk>
2017-06-10 07:52:18 +0100

    input whiles dont work on appharbour

--- hello/Program.cs
+++ hello/Program.cs
@@ -16,8 +16,8 @@ namespace hello
             aTimer.Interval = 5000;
             aTimer.Enabled = true;
 
-            Console.WriteLine("Press \'q\' to quit the sample.");
-            while (Console.Read() != 'q') ;
+           
+            while (false) ;
         }
 
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
