87e9bc93612e110631c9ea02f6f00a2a65bc3b91
Chris <ccooper4@hotmail.co.uk>
2017-06-10 07:49:39 +0100

    timers test mono

--- hello/Program.cs
+++ hello/Program.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace hello
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("hi");
-            for(int i= 0; i < 100; i++)
-            {
-                Console.WriteLine("testing appharbour.com");
-            }
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("timers test for mono deployment");
         }
     }
 }
